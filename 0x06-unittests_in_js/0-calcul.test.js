5e5d56bb10cd4e7b87f833d5a6b5cdf5ef048163
IbrahemGafour <ibrahem.abdelfatah.g4.com>
2024-12-30 09:44:14 +0000

    use webterm

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,28 +1,13 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('not rounded', () => {
-    assert.strictEqual(calculateNumber(1, 1), 2);
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-
-  it('has decimal point >= 0.5 ', () => {
-    assert.strictEqual(calculateNumber(1.5, 1), 3);
-    assert.strictEqual(calculateNumber(1, 2.6), 4);
-    assert.strictEqual(calculateNumber(1.5, 2.6), 5);
-    assert.strictEqual(calculateNumber(1.4, 2.6), 4);
-  });
-
-  it('one arg has decimal point >= 0.5 ', () => {
-    assert.strictEqual(calculateNumber(1.4, 1), 2);
-    assert.strictEqual(calculateNumber(1, 2.1), 3);
-    assert.strictEqual(calculateNumber(1.4, 1.4), 2);
-  });
-
-  it('should return NaN', () => {
-    assert.strictEqual(calculateNumber(), NaN);
-    assert.strictEqual(calculateNumber(1, 'e'), NaN);
-    assert.strictEqual(calculateNumber('e', 'e'), NaN);
-  });
+    it('should return 4 for calculateNumber(1, 3)', () => {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+    it('should return 5 for calculateNumber(1, 3.7)', () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+    // Add more test cases as needed
 });
+
